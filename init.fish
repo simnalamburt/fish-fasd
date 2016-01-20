c49f49aa6839a990e5f386dec274fe30c6b25788
Hyeon Kim <simnalamburt@gmail.com>
2016-01-21 02:19:49 +0900

    Fun fasd in synchronous way
    Reference:
      https://github.com/simnalamburt/plugin-fasd/issues/1

--- init.fish
+++ init.fish
@@ -3,9 +3,9 @@ if type -q fasd
   # Hook into fish preexec event
   function __fasd_run -e fish_preexec
     if type -q disown
-      command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &; disown
+      command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1; disown
     else
-      command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &
+      command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1
     end
   end
 else
